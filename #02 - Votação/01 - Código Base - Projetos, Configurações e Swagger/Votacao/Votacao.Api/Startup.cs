bd1c793032cbb172ce02440e4f324b8b6cd9b1b1
Lucas Santos <lucas.santos@otacom.com.br>
2021-01-28 19:41:11 -0300

    Swagger e configurações

--- #02 - Votação/01 - Código Base - Projetos, Configurações e Swagger/Votacao/Votacao.Api/Startup.cs
+++ #02 - Votação/01 - Código Base - Projetos, Configurações e Swagger/Votacao/Votacao.Api/Startup.cs
@@ -1,30 +1,53 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Votacao.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region [+] Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                //c.DescribeAllEnumsAsStrings();
+                c.DescribeAllParametersInCamelCase();
+                c.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\Swagger.xml");
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Votação API",
+                    Description = "Projeto responsável por gerenciar uma votação de melhor filme",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Santos",
+                        Email = "l_santos@hotmail.com.br",
+                        Url = new Uri("https://github.com/lsantoss"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licença MIT",
+                        Url = new Uri("https://github.com/lsantoss/Treinamentos/blob/main/LICENSE"),
+                    }
+                });
+            });
+
+            #endregion
+
             services.AddControllers();
         }
 
@@ -35,6 +58,12 @@ namespace Votacao.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "VotacaoAPI");
+            });
 
             app.UseHttpsRedirection();
 
