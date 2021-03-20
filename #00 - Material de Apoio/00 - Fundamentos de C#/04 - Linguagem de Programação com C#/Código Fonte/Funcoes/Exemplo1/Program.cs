78d4fef6a2b041b1ccd95f2f7e8020b0cf00e35d
Lucas Santos <lucas.santos@otacom.com.br>
2021-03-20 14:33:45 -0300

    Material fundamentos c#

---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/04 - Linguagem de Programação com C#/Código Fonte/Funcoes/Exemplo1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirMenu();
+
+            int operacao = LerOperacao();
+
+            double numero1 = LerDouble();
+
+            double numero2 = LerDouble();
+
+            double resultado = Calcular(operacao, numero1, numero2);
+
+            Console.WriteLine($"O resultado é: {resultado}");
+            Console.ReadKey();
+        }
+
+        private static void ImprimirMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=========== Menu ===========");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine();
+        }
+
+        private static int LerOperacao()
+        {
+            Console.Write("Escolha a operação: ");
+            var operacao = Convert.ToInt32(Console.ReadLine());
+            return operacao;
+        }
+
+        private static double LerDouble()
+        {
+            Console.Write("Digite o número: ");
+            var numero = Convert.ToDouble(Console.ReadLine());
+            return numero;
+        }
+
+        private static double Calcular(int operacao, double numero1, double numero2)
+        {
+            double resultado = 0;
+
+            switch (operacao)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    break;
+                case 4:
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    Console.WriteLine("Operação incorreta!");
+                    break;
+            }
+
+            return resultado;
+        }
+    }
+}
