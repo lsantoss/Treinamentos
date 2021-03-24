ac67540e09cbc067ad80621023b910dcb794a94a
Lucas Santos <lucas.santos@otacom.com.br>
2021-03-24 00:39:36 -0300

    fundamentos c#

---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/05 - Strings/Código Fonte/ExemploGuid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExemploGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varGuid1 = Guid.NewGuid();
+            var varGuid2 = Guid.Parse("55d65a67-5403-4ae3-ae82-0ca9297d501b");
+            var varGuid3 = Guid.Empty;
+
+            Console.WriteLine(varGuid1);
+            Console.WriteLine(varGuid2);
+            Console.WriteLine(varGuid3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/05 - Strings/Código Fonte/ExemploInterpolacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExemploInterpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = "lucas";
+            var string1 = "C:\\Users\\lucas\\Documents";
+            var string2 = $"C:\\Users\\{usuario}\\Documents";
+            var string3 = $@"C:\Users\{usuario}\Documents";
+
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
+            Console.WriteLine(string3);
+
+            Console.ReadKey();
+        }
+    }
+}
