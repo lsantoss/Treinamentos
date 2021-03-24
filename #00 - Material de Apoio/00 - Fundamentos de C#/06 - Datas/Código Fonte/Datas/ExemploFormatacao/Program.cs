b99565fbcdcd1165c41609aa64c1abb1754e4c48
Lucas Santos <lucas.santos@otacom.com.br>
2021-03-24 01:40:57 -0300

    fundamentos de c#

---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/06 - Datas/Código Fonte/Datas/ExemploFormatacao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploFormatacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine(date1);
+            Console.WriteLine(date1.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            Console.ReadKey();
+        }
+    }
+}
