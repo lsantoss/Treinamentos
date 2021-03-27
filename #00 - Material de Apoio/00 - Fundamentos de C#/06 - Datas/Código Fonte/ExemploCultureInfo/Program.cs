748a463e62b8b0d446b4d8db37ec1b8bd77692f1
Lucas Santos <lucas.santos@otacom.com.br>
2021-03-27 12:11:19 -0300

    fundamentos de c#

---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/06 - Datas/Código Fonte/ExemploCultureInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExemploCultureInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            DateTime date1 = DateTime.Now;
+
+            Console.WriteLine("====== Sem formatação ======");
+            Console.WriteLine(date1);
+            Console.WriteLine();
+
+            Console.WriteLine("====== pt-BR ======");
+            Console.WriteLine(date1.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine();
+
+            Console.WriteLine("====== en-US ======");
+            Console.WriteLine(date1.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
