5c477dcb9673e2aa54f39663462fdc28811bdfe0
Lucas Santos <lucas.santos@otacom.com.br>
2021-03-28 22:29:25 -0300

    fundamentos de c#

---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/08 - Exceptions/Código Fonte/Exemplo/ExemploExceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExemploExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 100;
+            int numero2 = 0;
+            double resultado = 0;
+
+            try
+            {
+                resultado = numero1 / numero2;
+            }
+            catch (Exception e)
+            {
+                //tratar
+                //logar erro memória
+                //logar erro xml
+                //logar erro base dados
+                //enviar email
+                //lançar Exception
+                throw e;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ #00 - Material de Apoio/00 - Fundamentos de C#/08 - Exceptions/Código Fonte/Exemplo/ExemploTryCatchFinally/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExemploTryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 100;
+            int numero2 = 5;
+            double resultado = 0;
+
+            try
+            {
+                resultado = numero1 / numero2;
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine($"Erro aritmético: {e.Message}");
+                Console.WriteLine();
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro genérico: {e.Message}");
+                Console.WriteLine();
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Resultado: {resultado}");
+                Console.WriteLine($"Esse bloco é executado independente de sucesso ou falha");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
